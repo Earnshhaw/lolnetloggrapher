c58361a09a3b702a0f3bba58f97f476ff09ba201
Earnshhaw <1Earnshaw@proton.me>
2025-08-03 17:44:19 +0200

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+mod netlog_parser;
+mod new_plotter;
+use anyhow::Result;
+use netlog_parser::{get_param_vec, get_timeloss, get_timeping};
+use new_plotter::graphit;
+use std::io;
+
+fn io() -> Result<String> {
+    let mut inp = String::new();
+    io::stdin().read_line(&mut inp)?;
+    Ok(inp.trim().to_string())
+}
+
+fn main() -> Result<()> {
+    //Take in the file name
+    println!("Copy paste path to network log file");
+    let iol = io()?;
+    let target = iol.as_str();
+    ///////////////////////
+
+    //Take in operation type
+    let paramms = get_param_vec(target)?;
+    println!("(P)ing over time");
+    println!("(L)oss over time");
+    let operation = io()?;
+    let ex = operation.trim();
+    ////////////////////////
+
+    //Match Ping and Loss to function calls or call an exception
+    if ex.to_lowercase() == "p" {
+        let tmpng = get_timeping(&paramms)?;
+        //visualize(tmpng, "#35C788", "Ping")?;
+        graphit(tmpng, "Ping")?;
+    } else if ex.to_lowercase() == "l" {
+        let lstm = get_timeloss(&paramms)?;
+        //visualize(lstm, "#DD3355", "Loss")?;
+        graphit(lstm, "Loss")?;
+    } else {
+        println!("Invalid operation type");
+    }
+    Ok(())
+}
---
+++ src/new_plotter.rs
@@ -0,0 +1,67 @@
+use anyhow::Result;
+use opener::open;
+use plotters::prelude::*;
+use rand::Rng;
+//use std::env;
+use std::path::Path;
+
+pub fn graphit(timepl: Vec<(f64, f64)>, optype: &str) -> Result<()> {
+    // Infer axis ranges from data
+    let (x_min, x_max) = timepl
+        .iter()
+        .map(|(x, _)| *x)
+        .fold((f64::MAX, f64::MIN), |(min, max), val| {
+            (min.min(val), max.max(val))
+        });
+
+    let (y_min, y_max) = timepl
+        .iter()
+        .map(|(_, y)| *y)
+        .fold((f64::MAX, f64::MIN), |(min, max), val| {
+            (min.min(val), max.max(val))
+        });
+
+    //let exe_dir = env::current_exe()
+    //.ok()
+    //.and_then(|p| p.parent().map(|p| p.to_path_buf()))
+    //.expect("Failed to determine executable directory");
+    //let file_path = exe_dir.join("Graph.png");
+    let file_path = Path::new("Graph.png");
+    let mut title = String::from("Graph");
+    if file_path.exists() {
+        let mut randoms = rand::rng();
+        let rndint: u16 = randoms.random_range(0..1000);
+        println!("{}", rndint);
+        title.push_str(rndint.to_string().as_str());
+        title.push_str(".png");
+        println!("{}", title)
+    } else {
+        title.push_str(".png");
+        println!("{}", title);
+    }
+
+    let root = BitMapBackend::new(&title, (640, 480)).into_drawing_area();
+    root.fill(&WHITE)?;
+
+    let mut chart = ChartBuilder::on(&root)
+        .caption(format!("{} over time", optype), ("sans-serif", 30))
+        .margin(10)
+        .x_label_area_size(40)
+        .y_label_area_size(40)
+        .build_cartesian_2d(x_min..x_max, y_min..y_max + 1 as f64)?;
+
+    chart
+        .configure_mesh()
+        .x_desc("Time (minutes)")
+        .y_desc(match optype {
+            "Ping" => "Ping (ms)",
+            "Loss" => "Loss (%)",
+            _ => "Value",
+        })
+        .draw()?;
+
+    chart.draw_series(LineSeries::new(timepl, &RED))?;
+    open(&title)?;
+
+    Ok(())
+}
