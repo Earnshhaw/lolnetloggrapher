5750fdb600276157ed784ab7fd37138bb85c670c
Earnshhaw <1Earnshaw@proton.me>
2025-08-03 23:01:22 +0200

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+mod config_reader;
 mod netlog_parser;
 mod new_plotter;
 use anyhow::Result;
 use netlog_parser::{get_param_vec, get_timeloss, get_timeping};
 use new_plotter::graphit;
 use std::io;
+
+use crate::config_reader::get_cfg;
 
 fn io() -> Result<String> {
     let mut inp = String::new();
@@ -29,12 +32,12 @@ fn main() -> Result<()> {
     //Match Ping and Loss to function calls or call an exception
     if ex.to_lowercase() == "p" {
         let tmpng = get_timeping(&paramms)?;
-        
-        graphit(tmpng, "Ping")?;
+        //visualize(tmpng, "#35C788", "Ping")?;
+        graphit(tmpng, "Ping", get_cfg()?)?;
     } else if ex.to_lowercase() == "l" {
         let lstm = get_timeloss(&paramms)?;
-    
-        graphit(lstm, "Loss")?;
+        //visualize(lstm, "#DD3355", "Loss")?;
+        graphit(lstm, "Loss", get_cfg()?)?;
     } else {
         println!("Invalid operation type");
     }
